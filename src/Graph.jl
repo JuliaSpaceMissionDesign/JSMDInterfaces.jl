d20cedcd5e5cea3574ed68b7e1dbffbc363f0536
MicheleCeresoli <michele.ceresoli@polimi.it>
2023-11-23 16:29:51 +0100

    Fixed imports

--- src/Graph.jl
+++ src/Graph.jl
@@ -2,7 +2,21 @@ module Graph
 
 using JSMDInterfaces.Interface
 
-import Graphs
+import Graphs:
+    AbstractGraph,
+    has_vertex, 
+    has_edge, 
+    edges, 
+    edgetype, 
+    inneighbors, 
+    ne, 
+    nv, 
+    outneighbors, 
+    vertices, 
+    is_directed, 
+    add_vertex!, 
+    add_edge!, 
+    has_path
 
 """    
     AbstractJSMDGraph{T}
@@ -14,7 +28,7 @@ Graphs here could be compatible with both JSMD ecosystem and [`Graphs.jl`](https
  - For `JSMD` compatibility, see also: [`get_path`](@ref).
  - For `Graphs.jl` compatibility, see also: [Graphs.jl interface](https://juliagraphs.org/Graphs.jl/dev/ecosystem/interface/)
 """
-abstract type AbstractJSMDGraph{T} <: Graphs.AbstractGraph{T} end 
+abstract type AbstractJSMDGraph{T} <: AbstractGraph{T} end 
 
 
 """    
@@ -29,25 +43,25 @@ abstract type AbstractJSMDGraphNode end
 # Graphs interfaces
 # ==================
 
-@interface function Graphs.has_vertex(::AbstractJSMDGraph, ::Int) end 
+@interface function has_vertex(::AbstractJSMDGraph, ::Int) end 
 
-@interface function Graphs.has_edge(::AbstractJSMDGraph, ::Int, ::Int) end
+@interface function has_edge(::AbstractJSMDGraph, ::Int, ::Int) end
 
-@interface function Graphs.edges(::AbstractJSMDGraph) end 
+@interface function edges(::AbstractJSMDGraph) end 
 
-@interface function Graphs.edgetype(::AbstractJSMDGraph) end 
+@interface function edgetype(::AbstractJSMDGraph) end 
 
-@interface function Graphs.inneighbors(::AbstractJSMDGraph, ::Int) end 
+@interface function inneighbors(::AbstractJSMDGraph, ::Int) end 
 
-@interface function Graphs.ne(::AbstractJSMDGraph) end 
+@interface function ne(::AbstractJSMDGraph) end 
 
-@interface function Graphs.nv(::AbstractJSMDGraph) end 
+@interface function nv(::AbstractJSMDGraph) end 
 
-@interface function Graphs.outneighbors(::AbstractJSMDGraph, ::Int) end 
+@interface function outneighbors(::AbstractJSMDGraph, ::Int) end 
 
-@interface function Graphs.vertices(::AbstractJSMDGraph) end 
+@interface function vertices(::AbstractJSMDGraph) end 
 
-@interface function Graphs.is_directed(::AbstractJSMDGraph) end 
+@interface function is_directed(::AbstractJSMDGraph) end 
 
 
 # JSMD interfaces
@@ -58,21 +72,21 @@ abstract type AbstractJSMDGraphNode end
 
 This function adds `vertex` to the graph `g`.
 """
-@interface function Graphs.add_vertex!(::AbstractJSMDGraph, ::N) where {N<:AbstractJSMDGraphNode} end 
+@interface function add_vertex!(::AbstractJSMDGraph, ::N) where {N<:AbstractJSMDGraphNode} end 
 
 """
     add_edge!(g::AbstractJSMDGraph, from::Int, to::Int, cost::Int)
 
 This function add an edge between `from` and `to` with weight `cost`.
 """
-@interface function Graphs.add_edge!(::AbstractJSMDGraph, ::Int, ::Int, ::Int) end 
+@interface function add_edge!(::AbstractJSMDGraph, ::Int, ::Int, ::Int) end 
 
 """
     has_path(g::AbstractJSMDGraph, from::Int, to::Int)
 
 Return true if there is a path between `from` and `to` in the graph `g`.
 """
-@interface function Graphs.has_path(::AbstractJSMDGraph, ::Int, ::Int) end 
+@interface function has_path(::AbstractJSMDGraph, ::Int, ::Int) end 
 
 """
     get_path(g::AbstractJSMDGraph, from::Int, to::Int)
